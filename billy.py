084698ce12b7cb7f4ab525bbe5054effcd08e9e3
Dave Niewinski <david_0429@hotmail.com>
2017-09-30 03:18:15 -0400

    Added motor control (untested).

--- billy.py
+++ billy.py
@@ -1,5 +1,21 @@
 from flask import Flask
 from flask import request
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+import time
+import atexit
+
+mh = Adafruit_MotorHAT(addr=0x60)
+
+def turnOffMotors():
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+myMotor = mh.getMotor(1)
 
 app = Flask(__name__)
 
@@ -14,6 +30,20 @@ def web_interface():
 def set_speed():
   speed = request.args.get("speed")
   print "Received " + str(speed)
+
+  direction = Adafruit_MotorHAT.FORWARD
+  if speed < 0:
+    direction = Adafruit_MotorHAT.BACKWARD
+
+  myMotor.setSpeed(abs(int(speed)))
+
   return "Received " + str(speed)
 
-app.run(host= '0.0.0.0')
+def main():
+  myMotor.setSpeed(150)
+  myMotor.run(Adafruit_MotorHAT.FORWARD);
+  myMotor.run(Adafruit_MotorHAT.RELEASE);
+
+  app.run(host= '0.0.0.0')
+
+main()
